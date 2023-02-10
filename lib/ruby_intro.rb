ef83755df5e64ef0d515b4448fd2106c7fbd74cd
Casey Quinn <74877883+cquinn2020@users.noreply.github.com>
2023-02-10 11:15:18 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,96 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |i|
+    sum += i
+  end
+  return sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    arr.sort!
+    return arr[-1] + arr[-2]
+  end
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  if arr.empty?
+    return false
+  elsif arr.length == 1 
+    return false
+  else
+    arr.each do |i|
+      arr.each do |j|
+        if i + j == number and i != j
+          return true
+        end
+      end
+    end
+  end
+  return false
 end
+
+
+
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  if string.empty?
+    return false
+  else
+    if string.start_with?("A", "E", "I", "O", "U", "a", "e", "i", "o", "u")
+      return false
+    else
+      if string[0] =~ /[[:alpha:]]/
+        return true
+      else
+        return false
+      end
+    end
+  end
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  if string =~ /^[01]+$/
+    string = string.to_i(2)
+    if string % 4 == 0
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    if isbn.empty? or price <= 0
+      raise ArgumentError
+    else
+      @isbn = isbn
+      @price = price
+    end
+  end
+  
+  def price_as_string
+    return "$" + '%.2f' % @price
+  end
 end
+
